55ad3716289a0f49b5ff1cb64520e098c09dfe82
Rahul Sunar <rahulsunarrahul55@gmail.com>
2025-03-27 22:51:10 +0530

    third

--- Home/views.py
+++ Home/views.py
@@ -4,7 +4,7 @@ from .models import blogs
 # Create your views here.
 def home(request):
     blog = blogs.objects.all()
-    return render(request,'home.html',{'blog':blog})
+    return render(request, 'Home/home.html', {'blog': blog})
 def blogs_details(request, blog_id):
     blog = get_object_or_404(blogs, id=blog_id)
     return render(request, 'blogs.html', {'post': blog})
